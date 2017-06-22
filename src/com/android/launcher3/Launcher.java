a1fa86c347f5af8b86304f7bc916a8ea65633773
Nick Reuter <nreuter85@gmail.com>
2017-06-21 21:47:20 -0500

    Check if pickerPackage is enabled before setting the package in the intent
    Partial revert of 04cc3a7beabf03c2e09c3b3e99601fe983a3d897
    
    If a WallpaperPicker is not installed Launcher3 will crash. This will allow the action
    to fallback to the users choice. Fixes the following error:
    
    06-17 20:30:04.282 5195 5195 E AndroidRuntime: FATAL EXCEPTION: main
    06-17 20:30:04.282 5195 5195 E AndroidRuntime: Process: com.android.launcher3, PID: 5195
    06-17 20:30:04.282 5195 5195 E AndroidRuntime: android.content.ActivityNotFoundException: No Activity found to handle Intent { act=android.intent.action.SET_WALLPAPER pkg=com.android.documentsui bnds=[105,1560][395,1765] (has extras) }
    06-17 20:30:04.282 5195 5195 E AndroidRuntime: at android.app.Instrumentation.checkStartActivityResult(Instrumentation.java:1809)
    06-17 20:30:04.282 5195 5195 E AndroidRuntime: at android.app.Instrumentation.execStartActivity(Instrumentation.java:1523)
    06-17 20:30:04.282 5195 5195 E AndroidRuntime: at android.app.Activity.startActivityForResult(Activity.java:4229)
    06-17 20:30:04.282 5195 5195 E AndroidRuntime: at com.android.launcher3.Launcher.startActivityForResult(Launcher.java:2010)
    06-17 20:30:04.282 5195 5195 E AndroidRuntime: at com.android.launcher3.Launcher.onClickWallpaperPicker(Launcher.java:2674)
    06-17 20:30:04.282 5195 5195 E AndroidRuntime: at com.android.launcher3.Launcher$13.onClick(Launcher.java:1399)
    06-17 20:30:04.282 5195 5195 E AndroidRuntime: at android.view.View.performClick(View.java:5637)
    06-17 20:30:04.282 5195 5195 E AndroidRuntime: at android.view.View$PerformClick.run(View.java:22429)
    06-17 20:30:04.282 5195 5195 E AndroidRuntime: at android.os.Handler.handleCallback(Handler.java:751)
    06-17 20:30:04.282 5195 5195 E AndroidRuntime: at android.os.Handler.dispatchMessage(Handler.java:95)
    06-17 20:30:04.282 5195 5195 E AndroidRuntime: at android.os.Looper.loop(Looper.java:154)
    06-17 20:30:04.282 5195 5195 E AndroidRuntime: at android.app.ActivityThread.main(ActivityThread.java:6208)
    06-17 20:30:04.282 5195 5195 E AndroidRuntime: at java.lang.reflect.Method.invoke(Native Method)
    06-17 20:30:04.282 5195 5195 E AndroidRuntime: at com.android.internal.os.ZygoteInit$MethodAndArgsCaller.run(ZygoteInit.java:891)
    06-17 20:30:04.282 5195 5195 E AndroidRuntime: at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:781)
    
    Change-Id: Iee5a93e5a932fc0e62ede5f6113091099ea99d99

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -2642,17 +2642,15 @@ public class Launcher extends Activity
         }
 
         String pickerPackage = getString(R.string.wallpaper_picker_package);
-        if (TextUtils.isEmpty(pickerPackage)) {
-            pickerPackage =  PackageManagerHelper.getWallpaperPickerPackage(getPackageManager());
-        }
-
         int pageScroll = mWorkspace.getScrollForPage(mWorkspace.getPageNearestToCenterOfScreen());
         float offset = mWorkspace.mWallpaperOffset.wallpaperOffsetForScroll(pageScroll);
 
         setWaitingForResult(new PendingRequestArgs(new ItemInfo()));
-        Intent intent = new Intent(Intent.ACTION_SET_WALLPAPER)
-                .setPackage(pickerPackage)
-                .putExtra(Utilities.EXTRA_WALLPAPER_OFFSET, offset);
+        Intent intent = new Intent(Intent.ACTION_SET_WALLPAPER);
+        if (PackageManagerHelper.isAppEnabled(getPackageManager(), pickerPackage)) {
+            intent.setPackage(pickerPackage);
+        }
+        intent.putExtra(Utilities.EXTRA_WALLPAPER_OFFSET, offset);
         intent.setSourceBounds(getViewBounds(v));
         startActivityForResult(intent, REQUEST_PICK_WALLPAPER, getActivityLaunchOptions(v));
     }
@@ -2664,7 +2662,7 @@ public class Launcher extends Activity
     public void onClickSettingsButton(View v) {
         if (LOGD) Log.d(TAG, "onClickSettingsButton");
         Intent intent = new Intent(Intent.ACTION_APPLICATION_PREFERENCES)
-                .setPackage(getPackageName());
+               //.setPackage(getPackageName());
         intent.setSourceBounds(getViewBounds(v));
         startActivity(intent, getActivityLaunchOptions(v));
     }
